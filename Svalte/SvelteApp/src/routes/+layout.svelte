444df13f2136ce1d5fe951ff2106a6a978386af4
Mohan Prajapati <mohanprajapati0203@outlook.com>
2023-01-02 18:33:10 +0530

    Added fetch recepies

---
+++ Svalte/SvelteApp/src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Footer from "../components/Footer.svelte";
+import Nav from "../components/Nav.svelte";
+
+</script>
+<Nav/>
+
+<slot>
+
+</slot>
+
+<Footer/>
--- Svalte/SvelteApp/src/routes/+page.svelte
+++ Svalte/SvelteApp/src/routes/+page.svelte
@@ -1,3 +1,40 @@
-<h1>Welcome to your library project</h1>
-<p>Create your package using @sveltejs/package and preview/showcase your work with SvelteKit</p>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- <script context="module">
+
+    /**@type {import('@sveltejs/kit').Load}*/
+    async function load(page){
+     return {
+        props:{
+            recipes:[{
+                title:"hello",
+                image:"image"
+            }]
+           
+        }
+     }
+    }
+
+</script>
+
+<script>
+	import { onMount } from "svelte";
+
+    export let title;
+   
+    let data=[];
+ onMount(async()=>{
+    console.log(title);
+    // const res = await fetch("https://api.spoonacular.com/recipes/complexSearch?apiKey=e6363c89279d46eca0c80ce53ed34331");
+    // const {results} = await res.json();
+    // data = results;
+    // console.log(data);
+ })
+</script>
+
+<h1>Food Recepies</h1>
+{title}
+{#each data as item}
+<div>
+    <p>{item.title}</p>
+    <img src={item.image} alt={item.title}/>
+</div>
+{/each} -->
